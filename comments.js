d5f9208bdd95c0d4a5a4ea5cf40a7d1a7d67b90f
Ravindu Malshan <ravindumalshan1111@gmail.com>
2025-02-24 04:57:58 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -19,3 +19,18 @@ app.listen(PORT, () => {
 });
 
 // run the server with node comments.js
+
+// handle graceful shutdown
+process.on('SIGTERM', () => {
+    console.log('SIGTERM signal received: closing HTTP server');
+    server.close(() => {
+        console.log('HTTP server closed');
+    });
+});
+
+process.on('SIGINT', () => {
+    console.log('SIGINT signal received: closing HTTP server');
+    server.close(() => {
+        console.log('HTTP server closed');
+    });
+});
