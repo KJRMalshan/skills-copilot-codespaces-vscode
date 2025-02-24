d42a4a6bb4b6bbb0d2dbf4bf4937f2c1a9196be6
Ravindu Malshan <ravindumalshan1111@gmail.com>
2025-02-24 04:49:17 +0000

    copilot third commit

---
+++ comments.js
@@ -0,0 +1,20 @@
+// create web server
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+// create a route
+app.get('/', (req, res) => {
+    res.send('Welcome to the homepage');
+});
+
+app.get('/comments', (req, res) => {
+    res.send('Welcome to the comments page');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// run the server with node comments.js
